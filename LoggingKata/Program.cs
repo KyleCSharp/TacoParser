b12801dd6467395428c91c5fc9ad3b7b49a3815e
Kyle Rigler <kylelrigler@gmail.com>
2022-10-16 16:07:50 -0400

    moved instance of cora and corb to save computer resources

--- LoggingKata/Program.cs
+++ LoggingKata/Program.cs
@@ -41,6 +41,8 @@ namespace LoggingKata
             ITrackable tacobell1 = null;
             ITrackable tacobell2 = null;
             double distance = 0;
+            var corA = new GeoCoordinate();//put outside to save performace
+            var corB = new GeoCoordinate();//outside to save performace
             // -done for me- Include the Geolocation toolbox, so you can compare locations: `using GeoCoordinatePortable;`
 
             //HINT NESTED( means using loop in a loop) LOOPS SECTION---------------------
@@ -48,7 +50,6 @@ namespace LoggingKata
             for (int i = 0; i < locations.Length; i++)
             {
                 var locA = locations[i];
-                var corA = new GeoCoordinate();
                 corA.Latitude = locA.Location.Latitude;
                 corA.Longitude= locA.Location.Longitude;
 
@@ -56,7 +57,6 @@ namespace LoggingKata
                 for (int j = 0; j < locations.Length; j++)
                 {
                     var locB = locations[j];
-                    var corB = new GeoCoordinate();
                     corB.Latitude= locB.Location.Latitude;
                     corB.Longitude=locB.Location.Longitude;
                     if ( corA.GetDistanceTo(corB) > distance)
@@ -92,8 +92,8 @@ namespace LoggingKata
 
             // Once you've looped through everything, you've found the two Taco Bells farthest away from each other.
 
-            logger.LogInfo($"{tacobell1.Name} and {tacobell2.Name} are the farthest apart.....");
-            
+            logger.LogInfo($"{tacobell1.Name} AND {tacobell2.Name} are the farthest apart.....from one another they are                 {distance/1609.344 } miles apart");
+            //logger.LogInfo($"{distance/1609.344} miles apart  ");
         }
     }
 }
